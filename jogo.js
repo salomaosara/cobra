e3047d0e2253cace380f1ea3eeff0afed0ce4054
sarasalomao <salomaosara03@gmail.com>
2025-07-09 12:00:58 -0300

    Atualização jogo da cobra

--- jogo.js
+++ jogo.js
@@ -6,11 +6,33 @@ function jogar(){
     fruta.desenhar();
     if (fruta.teveColisao(cobra)){
         placar.pontuacao+=fruta.valor;
+        cobra.crescer();
+        comida.play();
         fruta = new Fruta (10)
     }
     if (cobra.vida > 0)
     requestAnimationFrame(jogar)
-}
+    }
+    {
+        trilha.pause();
+        trilha.currentTime=0;
+        gameover.play();
+        placar.nomeJogo = "Fim de Jogo. Clique na tela para jogar...";
+        placar.desenhar()
+    }
+
+
+const trilha = new Audio();
+trilha.src= "musicajogo.mp3";
+const comida = new Audio();
+comida.src= "musicamastigação.mp3";
+const gameover = new Audio();
+gameover.src= "musicagameover.mp3";
+let apple = new Apple(10);
+placar.nomeJogo="Clique na tela para começar o jogo..."
+placar.desenhar();
+tela.desenhar();
+
 let fruta = new Fruta(10);
 jogar();
  
